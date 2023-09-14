13d47f4262dcecf3504eb2c9f52e4d95f61593be
kurtschambach <kurt.schambach@gmail.com>
2023-09-14 19:08:22 +0200

    feat: added google verifacation tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,14 +5,14 @@ import { Metadata } from "next";
 import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
-	title: "Kurt Schambach",
-	description: "Software engineer at Titanom Technologies, PlantOrg and PyShop ",
+	title: "a3chron - Kurt Schambach",
+	description: "Hi, my name is Kurt Schambach. I'm a Software engineer at Titanom Technologies, working on some of my own Projects in free time",
 	openGraph: {
 		title: "Kurt Schambach",
 		description:
-			"Software engineer",
+			"Hi, my name is Kurt Schambach. I'm a Software engineer at Titanom Technologies, working on some of my own Projects in free time",
 		url: "https://kurtschambach.vercel.app/",
-		siteName: "Kurt Schambach - Portfolio",
+		siteName: "a3chron - Kurt Schambach - Portfolio",
 		locale: "de-DE",
 		type: "website",
 	},
@@ -48,6 +48,9 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
+			<head>
+				<meta name="google-site-verification" content="waG0eqk7cOJVSMbB42gHQIR-bDsRmy5ABoQWNFI8UKQ" />
+			</head>
 			<body
 				className={`bg-emerald-800 ${
 					process.env.NODE_ENV === "development" ? "debug-screens" : undefined
