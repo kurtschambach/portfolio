fdbb3641ffb6227fbb8b7031895202cab870e135
kurtschambach <kurt.schambach@gmail.com>
2024-01-06 15:01:38 +0100

    feat: improved ProjectsCard

--- app/components/projects-card.tsx
+++ app/components/projects-card.tsx
@@ -5,25 +5,22 @@ const ProjectsCard = () => {
 		<div className="font-bold bg-dark-bg z-10 p-6 py-12 rounded-xl w-72 h-full duration-500 flex flex-col items-center justify-center gap-6 text-text hover:text-hover">
 			<h2 className="text-lg">Some of my Apps</h2>
 			<Link
-				href="https://plant-organizer.vercel.app"
-				target="_blank"
+				href="/projects/plant-org"
 				className="w-52 flex flex-row gap-2 items-center justify-center text-violet bg-violet/10 p-2 px-4 rounded-full border-2 border-violet"
 			>
 				Plant Org
 			</Link>
 			<Link
-				href="https://dev-apps.vercel.app"
-				target="_blank"
+				href="/projects/dev-apps"
 				className="w-52 flex flex-row gap-2 items-center justify-center text-violet bg-violet/10 p-2 px-4 rounded-full border-2 border-violet"
 			>
 				Dev Apps
 			</Link>
 			<Link
-				href="https://centaurii.vercel.app"
-				target="_blank"
+				href="/projects/productivity-app"
 				className="w-52 flex flex-row gap-2 items-center justify-center text-violet bg-violet/10 p-2 px-4 rounded-full border-2 border-violet"
 			>
-				Centauri
+				Productivity App
 			</Link>
 		</div>
 	);
