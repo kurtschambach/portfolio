6d8738b37302e2dded58b97b12da21b51c68491b
kurtschambach <kurt.schambach@gmail.com>
2025-04-20 21:51:57 +0200

    feat: add blog link

--- app/api/shields/get-latest-blog/route.ts
+++ app/api/shields/get-latest-blog/route.ts
@@ -3,7 +3,9 @@ import { getSortedArticlesbyDate } from "@/util/utils";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-	const sortedArticles = getSortedArticlesbyDate(allArticles);
+	const sortedArticles = getSortedArticlesbyDate(
+		allArticles.filter((article) => article.published),
+	);
 	if (sortedArticles.length < 1) {
 		console.error("Failed to get sorted articles");
 		return NextResponse.json(
@@ -14,19 +16,23 @@ export async function GET() {
 				message: "No Posts",
 				color: "#f38ba8",
 				labelColor: "#313244",
+				link: "https://a3chron.vercel.app/blog",
 			},
 			{ status: 404 },
 		);
 	}
+
+	const latestArticle = sortedArticles[0];
 
 	return NextResponse.json(
 		{
 			schemaVersion: 1,
 			style: "for-the-badge",
 			label: "Latest Blog Post",
-			message: sortedArticles[0].title,
+			message: latestArticle.title,
 			color: "#fab387",
 			labelColor: "#313244",
+			link: `https://a3chron.vercel.app/blog/${latestArticle.slug}`,
 		},
 		{ status: 200 },
 	);
