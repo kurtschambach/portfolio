a1b413c7a929b9944a0c1bf10fe154a5fdad199b
kurtschambach <kurt.schambach@gmail.com>
2025-05-26 20:27:58 +0200

    fix: request prop

--- app/api/get-likes/[slug]/route.ts
+++ app/api/get-likes/[slug]/route.ts
@@ -2,7 +2,10 @@ import { NextResponse } from "next/server";
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
-export async function GET({ params }: { params: { slug: string } }) {
+export async function GET(
+  _request: Request,
+  { params }: { params: { slug: string } },
+) {
   const articleSlug = params.slug;
 
   const supabase_url = process.env.SUPABASE_URL;
