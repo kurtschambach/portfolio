b7b2c02e9dbd417bc8865e145314dacd694accb2
kurtschambach <kurt.schambach@gmail.com>
2025-07-18 22:47:12 +0200

    fix: small project styling

--- app/projects/layout.tsx
+++ app/projects/layout.tsx
@@ -5,7 +5,7 @@ export default function ProjectsLayout({
   children,
 }: { children: React.ReactNode }) {
   return (
-    <div className="relative h-full max-h-dvh overflow-y-auto bg-green">
+    <div className="relative h-full max-h-dvh overflow-y-auto bg-base">
       <Navigation className="bg-green" />
       {children}
     </div>
